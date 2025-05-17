625ed5d37f83ff8ac49905d6857eae0931c83945
giacarrea <cher_gia@hotmail.com>
2025-05-17 13:19:08 +0200

    fixed json event parsing & ui improvement

---
+++ EventBlazorApp/Models/EventDto.cs
@@ -0,0 +1,86 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EventBlazorApp.Models
+{
+    public class EventListDto
+    {
+        [JsonPropertyName("$id")]
+        public string Id { get; set; } = "";
+
+        [JsonPropertyName("$values")]
+        public List<EventDto> Events { get; set; } = new();
+    }
+
+    public class EventDto
+    {
+        [JsonPropertyName("$id")]
+        public string IdRef { get; set; } = "";
+
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; } = "";
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; } = "";
+
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
+
+        [JsonPropertyName("location")]
+        public string Location { get; set; } = "";
+
+        [JsonPropertyName("category")]
+        public string Category { get; set; } = "";
+
+        [JsonPropertyName("capacity")]
+        public int Capacity { get; set; }
+
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+
+        [JsonPropertyName("registrations")]
+        public RegistrationList RegistrationList { get; set; } = new();
+
+        // Not from API, used for UI logic
+        [JsonIgnore]
+        public bool IsRegistered { get; set; }
+    }
+
+    public class RegistrationList
+    {
+        [JsonPropertyName("$id")]
+        public string Id { get; set; } = "";
+
+        [JsonPropertyName("$values")]
+        public List<RegistrationDto> Registrations { get; set; } = new();
+    }
+
+    public class RegistrationDto
+    {
+        [JsonPropertyName("$id")]
+        public string IdRef { get; set; } = "";
+
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("eventId")]
+        public Guid EventId { get; set; }
+
+        [JsonPropertyName("event")]
+        public EventRefDto? Event { get; set; }
+
+        [JsonPropertyName("userId")]
+        public string UserId { get; set; } = "";
+
+        [JsonPropertyName("registeredAt")]
+        public DateTime RegisteredAt { get; set; }
+    }
+
+    public class EventRefDto
+    {
+        [JsonPropertyName("$ref")]
+        public string Ref { get; set; } = "";
+    }
+}
---
+++ EventBlazorApp/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EventBlazorApp.Models
+{
+    public class LoginModel
+    {
+        [JsonPropertyName("username")]
+        public string Username { get; set; } = "";
+
+        [JsonPropertyName("password")]
+        public string Password { get; set; } = "";
+    }
+}
